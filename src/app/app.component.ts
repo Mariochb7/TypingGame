fc081517a520d0cd8fb90c6bc31c244abe9b1bc6
Mario Chayeb <mariochb7@gmail.com>
2024-06-03 01:04:08 +0300

    First commit TypingApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { lorem, random } from 'faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TypingGame';
+  /* isSuccess = false; 1st sol*/
+  enteredText = '';
+  randomText = lorem.sentence();
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.enteredText = target.value;
+    /* this.isSuccess = value == this.randomText ? true : false; 1st sol*/
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
